b29265f7dc5814c52af789edbe5474f4c468b68d
Jordon Archer <jordon_services@hotmail.co.uk>
2023-06-23 07:41:40 +0100

    Logging UDP to csv

--- app.py
+++ app.py
@@ -1,38 +1,53 @@
-from flask import Flask, render_template
+import csv
+import threading
 import socket
-import asyncio
-
-# A simple database dictionary for storing measurements
-database = {}
+from flask import Flask, render_template
+from datetime import datetime
+from collections import defaultdict
 
 app = Flask(__name__)
 
-UDP_IP = "0.0.0.0"  # Listen on all available network interfaces
-UDP_PORT = 1234     # Port to listen on
+data_dict = defaultdict(str)  # Will store IPs as keys and most recent data as values
+fieldnames = ["Time"]  # List of fieldnames for the CSV, starting with "Time"
 
-def receive_udp():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((UDP_IP, UDP_PORT))
+def udp_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('0.0.0.0', 1234))
 
     while True:
-        data, addr = sock.recvfrom(1024)
-        data = data.decode()
+        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+        print("Received message:", data, "from:", addr)
 
-        print(data)
+        ip = addr[0]
+        if ip not in fieldnames:
+            fieldnames.append(ip)  # If IP is new, add it to the fieldnames
 
-        # # Create a JSON object with the matrix
-        #json_data = json.dumps(data)
-        # # print("Received message:")
-        #print(json_data)
+        for key in data_dict:  # Reset the dict for new row
+            data_dict[key] = ''
+        
+        data_dict[ip] = data  # Store most recent data
 
-        # socketio.emit('udp_message', json_data)  # Emit UDP message to connected clients
+        # Append the new row to the CSV
+        append_csv()
+
+def append_csv():
+    with open('udp_data.csv', 'a', newline='') as file:  # Open the file in append mode
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        if file.tell() == 0:  # Check if the file is empty
+            writer.writeheader()  # If it is, write the headers
+        writer.writerow({"Time": datetime.now().isoformat(), **data_dict})
+
 
 @app.route('/')
-def index():
-    return render_template('index.html', measurements=database)
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    import threading
-    udp_thread = threading.Thread(target=receive_udp)
-    udp_thread.start()
-    app.run(debug=True, port=5000)
+        # Write the CSV headers when the program starts
+    with open('udp_data.csv', 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+    t = threading.Thread(target=udp_server)
+    t.start()
+    app.run(debug=True)
